7ac873ab08433bbcdefd04ccf0de36764a997b1b
caiolimadesouza <caiolimadesouza8@gmail.com>
2026-03-26 19:46:35 -0300

    aula python

--- exercicio7.py
+++ exercicio7.py
@@ -1,1 +1,9 @@
 import math
+
+numero = float(input('num: '))
+
+if numero >= 0:
+    raiz = math.sqrt(numero)
+    print(f"raiz quadrade de {numero} é igual a {raiz}")
+else:
+    print("impossivel extrair raiz de numero negativo")
