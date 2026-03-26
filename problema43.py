6bd33044a025a29a974ea520e8b791d9a5690f21
caiolimadesouza <caiolimadesouza8@gmail.com>
2026-03-26 20:55:06 -0300

    aula python

---
+++ problema43.py
@@ -0,0 +1,6 @@
+N = int(input("N: "))
+
+i = 1
+while i <= N:
+    print(i)
+    i = i + 1
---
+++ tabuada.py
@@ -0,0 +1,18 @@
+n = int(input("informe N: "))
+
+i = 1
+
+
+print(f"{n} X {i} = {n * 1}")
+i = i + 1
+print(f"{n} X {i} = {n * 1}")
+i = i + 1
+print(f"{n} X {i} = {n * 1}")
+i = i + 1
+print(f"{n} X {i} = {n * 1}")
+i = i + 1
+print(f"{n} X {i} = {n * 1}")
+i = i + 1
+print(f"{n} X {i} = {n * 1}")
+i = i + 1
+
